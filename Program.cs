8dbb14e4628f9e2c4068948dd31e4081dd5c1d12
FlavioAcuña <flavio.acuna.t@gmail.com>
2023-10-06 10:55:58 -0300

    Changes to be committed: 	new file:   Ciclos.csproj 	new file:   Ciclos.sln 	new file:   Program.cs 	new file:   bin/Debug/net7.0/Ciclos.deps.json 	new file:   bin/Debug/net7.0/Ciclos.dll 	new file:   bin/Debug/net7.0/Ciclos.exe 	new file:   bin/Debug/net7.0/Ciclos.pdb 	new file:   bin/Debug/net7.0/Ciclos.runtimeconfig.json 	new file:   obj/Ciclos.csproj.nuget.dgspec.json 	new file:   obj/Ciclos.csproj.nuget.g.props 	new file:   obj/Ciclos.csproj.nuget.g.targets 	new file:   obj/Debug/net7.0/.NETCoreApp,Version=v7.0.AssemblyAttributes.cs 	new file:   obj/Debug/net7.0/Ciclos.AssemblyInfo.cs 	new file:   obj/Debug/net7.0/Ciclos.AssemblyInfoInputs.cache 	new file:   obj/Debug/net7.0/Ciclos.GeneratedMSBuildEditorConfig.editorconfig 	new file:   obj/Debug/net7.0/Ciclos.GlobalUsings.g.cs 	new file:   obj/Debug/net7.0/Ciclos.assets.cache 	new file:   obj/Debug/net7.0/Ciclos.csproj.AssemblyReference.cache 	new file:   obj/Debug/net7.0/Ciclos.csproj.CoreCompileInputs.cache 	new file:   obj/Debug/net7.0/Ciclos.csproj.FileListAbsolute.txt 	new file:   obj/Debug/net7.0/Ciclos.dll 	new file:   obj/Debug/net7.0/Ciclos.genruntimeconfig.cache 	new file:   obj/Debug/net7.0/Ciclos.pdb 	new file:   obj/Debug/net7.0/apphost.exe 	new file:   obj/Debug/net7.0/ref/Ciclos.dll 	new file:   obj/Debug/net7.0/refint/Ciclos.dll 	new file:   obj/project.assets.json 	new file:   obj/project.nuget.cache

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿for (int i = 1; i < 10; i++)
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine("---------While----------");
+int j = 1;
+while (j <= 10)
+{
+    Console.WriteLine(j);
+    j ++;
+}
+Console.WriteLine("--------Do While-----------");
+int k = 1;
+do
+{
+    Console.WriteLine(k);
+    k ++;
+} while (k <= 10);
